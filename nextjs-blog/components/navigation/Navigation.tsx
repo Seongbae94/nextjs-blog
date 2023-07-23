7714621738a396c7ede186e98af90575380ae118
Seongbae94 <seongbaelee944@gmail.com>
2023-07-23 18:12:00 +0900

    Feat: adding Navigation skeleton
    Added a basic Navigation skeleton

---
+++ nextjs-blog/components/navigation/Navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Link href="/">Explorer</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/cities">Cities</Link>
+          </li>
+          <li>
+            <Link href="/experiences">Experiences</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
