2a6b9a90ce0804888f0a728490e2e3668aa8d056
Seongbae94 <seongbaelee944@gmail.com>
2023-07-28 00:38:20 +0900

    feat: parsing body
    parsing body on post detail page body section

--- nextjs-blog/components/post/post-body.tsx
+++ nextjs-blog/components/post/post-body.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import parse from "html-react-parser";
 
 const PostBody = ({ body }: { body: string }) => {
-  return <div>{body}</div>;
+  const getParsedHTML = (body: string) => {
+    return parse(body);
+  };
+  return <div>{getParsedHTML(body)}</div>;
 };
 
 export default PostBody;
