337bc370280fcf46b7322667578cbca8a8829b6d
Seongbae94 <seongbaelee944@gmail.com>
2023-07-30 16:50:56 +0900

    feat: adding form submit feature
    added form submit feature and number of subscribers

--- nextjs-blog/components/elements/cta-card.tsx
+++ nextjs-blog/components/elements/cta-card.tsx
@@ -1,7 +1,34 @@
+import directus from "@/lib/directus";
+import { revalidatePath, revalidateTag } from "next/cache";
 import Image from "next/image";
 import React from "react";
 
-const CTACard = () => {
+const CTACard = async () => {
+  const formAction = async (formData: FormData) => {
+    "use server";
+    try {
+      const email = formData.get("email");
+      await directus.items("subscribers").createOne({
+        email,
+      });
+      revalidateTag("subscribers-count");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const subscribersCount = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}items/subscribers?meta=total_count&access_token=${process.env.ADMIN_TOKEN}`,
+    {
+      next: {
+        tags: ["subscribers-count"],
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res.meta.total_count)
+    .catch((error) => console.log(error));
+
   return (
     <div className="rounded-md bg-slate-100 py-10 px-6 relative overflow-hidden">
       <div className="absolute z-10 inset-0 bg-gradient-to-br from-white/95 via-white/70 to-white/30" />
@@ -21,8 +48,14 @@ const CTACard = () => {
           visited most of the great cities of America and currently I'm
           traveling in Korea. Join me!
         </p>
-        <form className="mt-6 flex items-center gap-2 w-full">
+        <form
+          key={subscribersCount + "subscribers-form"}
+          action={formAction}
+          className="mt-6 flex items-center gap-2 w-full"
+        >
           <input
+            type="email"
+            name="email"
             placeholder="write your email."
             className="md:w-auto w-full bg-white/80 text-base rounded-md placeholder:text-sm py-2 px-3 outline-none focus:ring-2 ring-neutral-600"
           />
@@ -30,6 +63,14 @@ const CTACard = () => {
             Sign Up
           </button>
         </form>
+
+        <div className="mt-5 text-neutral-700">
+          Join our{" "}
+          <span className="bg-neutral-700 rounded-md text-neutral-100 py-1 px-2 text-sm">
+            {subscribersCount}
+          </span>{" "}
+          subscribers now!
+        </div>
       </div>
     </div>
   );
