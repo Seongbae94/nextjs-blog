53c238b6070add6f3efa749595c78c9266e20d01
Seongbae94 <seongbaelee944@gmail.com>
2023-08-01 01:24:39 +0900

    feat: added feature to change language on navigation bar
    added feature to change language on navigation bar (language button)

--- nextjs-blog/components/navigation/Navigation.tsx
+++ nextjs-blog/components/navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import PaddingContainer from "../layout/padding-container";
 import { getDictionary } from "@/lib/getDictionary";
+import LangSwitcher from "./lang-switcher";
 
 const Navigation = async ({ locale }: { locale: string }) => {
   const dictionary = await getDictionary(locale);
@@ -15,6 +16,9 @@ const Navigation = async ({ locale }: { locale: string }) => {
           </Link>
           <nav>
             <ul className="flex items-center gap-4 text-neutral-600">
+              <li>
+                <LangSwitcher locale={locale} />
+              </li>
               <li>
                 <Link href={`/${locale}/cities`}>
                   {dictionary.navigation.links.cities}
